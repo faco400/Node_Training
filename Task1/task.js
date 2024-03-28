51e55851d18ec9cedd6c408af6b40d060602623c
faco400 <vd.souza1999@gmail.com>
2024-03-28 19:47:49 -0300

    Add task1 directory

---
+++ Task1/task.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+String.plus = function(string1, string2) {
+  console.log(`sum ${string1}, ${string2}`);
+};
+
+// String.minus = function (string) {
+//   console.log');
+// };
+
+// String.divide = function (string) {
+//   console.log('soma');
+// };
+
+// String.multiply = function(string) {
+//   console.log('soma');
+// };
+
+const num1 = prompt('Please, type first number');
+const num2 = prompt('Please, type second number');
+
+// String.plus(num1);
+
+// String.plus(param)
+
+String.plus(param1, param2)
+
+String.plus(param)
